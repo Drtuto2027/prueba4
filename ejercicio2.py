8c8ff3fab0109450d235eb9eb5c410043791133a
unknown <molinabertulfo26@gmail.com>
2025-06-13 10:01:35 -0500

    esta archivo contiene los comandos del ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+# Pedir la temperatura en grados Fahrenheit
+fahrenheit = float(input("Ingresa la temperatura en Fahrenheit: "))
+
+# Convertir a grados Celsius
+celsius = (5 / 9) * (fahrenheit - 32)
+
+# Mostrar el resultado
+print(f"Temperatura en grados Celsius: {celsius :.2f}")
