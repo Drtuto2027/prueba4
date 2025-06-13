d27cf869789eb2da5c88a5dfa8239714c37dc39d
unknown <molinabertulfo26@gmail.com>
2025-06-13 10:10:21 -0500

    se realizo cambios en el achivo ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,12 +1,12 @@
 # Tomar dos números enteros del usuario
 num1 = int(input("Ingresa el primer número entero: "))
 num2 = int(input("Ingresa el segundo número entero: "))
-
+num3 = int(input("Ingresa el segundo número entero: "))
 # Realizar las operaciones básicas
-suma = num1 + num2
-resta = num1 - num2
-multiplicacion = num1 * num2
-division = num1 / num2
+suma = num1 + num2 + num3
+resta = num1 - num2 . num3
+multiplicacion = num1 * num2 * num3
+division = num1 / num2 / num3
 
 # Mostrar los resultados
 print(f"Suma: {suma}, Resta: {resta}, Multiplicación: {multiplicacion}, Division: {division}")
