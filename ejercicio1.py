df6724f328a8f3551d55cac8e6def978435ddecd
unknown <molinabertulfo26@gmail.com>
2025-06-13 08:46:59 -0500

    este archivo contiene la logica de la operacion

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+# Tomar dos números enteros del usuario
+num1 = int(input("Ingresa el primer número entero: "))
+num2 = int(input("Ingresa el segundo número entero: "))
+
+# Realizar las operaciones básicas
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+# Mostrar los resultados
+print(f"Suma: {suma}, Resta: {resta}, Multiplicación: {multiplicacion}, Division: {division}")
+      
