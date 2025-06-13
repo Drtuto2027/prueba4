c48a0a18446bb854a3f2872dbad03e9b292057da
unknown <molinabertulfo26@gmail.com>
2025-06-13 10:04:19 -0500

    esta archivo contiene los comandos del ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,6 @@
+# pedir la edad delusuario
+edad=int(input("ingresa tu edad"))
+#verificar si es mayor de edad 
+es_mayor_de_edad = edad >18
+#mostrar eñ resultado
+print(f"¿eres mayor de edad:{es_mayor_de_edad}")
